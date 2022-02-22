cbcee7ff68ea747c47cef0eada3965ead05f0111
akash <akashlj2@gmail.com>
2022-02-22 13:34:25 +0530

    header finallized

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
       
         {/* <Login /> */}
 
-    <Footer />
+    {/* <Footer /> */}
   </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,29 +4,30 @@ import React from "react";
 export const Header = () => {
   return (
     <header className="">
-      <div className="top-[3px] width mx-auto flex items-center small-margin">
+      <div className=" width mx-auto flex items-center mt-[3px] pl-1 ">
         <div className="menu">
-          <button className="btn flex px-1  ">
+          <button className="btn flex px-1">
             <i className="fa fa-bars items-center " aria-hidden="true"></i>
           </button>
 
-          <div className="items-box flex-col items-center justify-center">
-            <div className="item bg-white px-2 py-1 border">Rewards</div>
-            <div className="item bg-white px-2 py-1 border">Offers</div>
-            <div className="item bg-white px-2 py-1 border" >Suggestions</div>
+          <div className="items-box flex flex-col items-center justify-center w-[50vw]">
+            <div className="item bg-white px-4 py-2 border">Rewards</div>
+            <div className="item bg-white px-4 py-2 border">Offers</div>
+            <div className="item bg-white px-4 py-2 border" >Suggestions</div>
           </div>
         </div>
+{/*logo declaration   */}
+        <div className="pl-[2px] pr-4 logo mr-auto relative  mt-[1px] ml-2  small-logo"></div>
 
-        <div className="logo mr-auto relative -top-[0.3rem] small-left"></div>
-        <div className="nav flex  gap-[0.83em] items-start flat-menu ">
-          <div className="rewards">Rewards</div>
-          <div className="offers">Offers</div>
-          <div className="suggestion">Suggestions</div>
-          <button className="flex border-box h-[32px] rounded bg-primary font-normal  items-center px-2 py-1 relative -top-[0.4rem]">
-            <span className="text-tertiary">Login / Sign In</span>
+        <div className="nav flex  gap-[0.9rem] items-center flat-menu ">
+          <div className="rewards ">Rewards</div>
+          <div className="offers ">Offers</div>
+          <div className="suggestion ">Suggestions</div>
+          <button className="flex border-box h-9 rounded bg-primary font-normal  p-[5px] items-center  relative  -top-[2px]">
+            <span className="text-tertiary lh-14">Login / Sign In</span>
           </button>
         </div>
-          <button className="flex border-box h-[32px] rounded bg-primary font-normal  items-center px-2 py-1 relative -top-[0.4rem] show-small">
+          <button className="flex border-box h-9 rounded bg-primary font-normal  items-center p-[5px] relative top-[2px] show-small">
             <span className="text-tertiary">Login / Sign In</span>
           </button>
       </div>
