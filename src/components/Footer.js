3a17aa3c421119666c160cbf9a71066079e7c770
akash <akashlj2@gmail.com>
2022-02-23 13:35:54 +0530

    footer complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,15 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer className="container border ">
-      <div className="flex items-center justify-center mx-auto px-6 py-1" ></div>
-      <div className="flex items-center justify-center bg-primary text-white mx-auto p-1">
-          <div className="flex flex-wrap">
-            <span className="inline-block py-1 px-2 text-[10px] text-tertiary flex-1">About Us</span>
-            <span className="inline-block py-1 px-2 text-[10px] text-tertiary flex-1">Contact Us</span>
-            <span className="inline-block py-1 px-2 text-[10px] text-tertiary flex-1"> Terms & Conditions</span>
-            <span className="inline-block py-1 px-2 text-[10px] text-tertiary flex-1">Privacy Policy</span>
-            <span className="inline-block py-1 px-2 text-[10px] text-tertiary flex-1">Refund Policy</span>
-          </div>
+    <footer className="border w-full">
+      <div className="flex items-center justify-center bg-primary text-white  px-2 py-1 w-full">
+          <ul className="flex flex-wrap justify-center  list-none w-full">
+            <li className="inline-block py-1 px-2 text-[10px] text-tertiary ">About Us</li>
+            <li className="inline-block py-1 px-2 text-[10px] text-tertiary ">Contact Us</li>
+            <li className="inline-block py-1 px-2 text-[10px] text-tertiary "> Terms & Conditions</li>
+            <li className="inline-block py-1 px-2 text-[10px] text-tertiary ">Privacy Policy</li>
+            <li className="inline-block py-1 px-2 text-[10px] text-tertiary ">Refund Policy</li>
+          </ul>
       </div>
     </footer>
   );
