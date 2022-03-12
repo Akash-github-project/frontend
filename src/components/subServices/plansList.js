3cdd5f019ecc41e9ab948a478d39350793ff19ae
Akash-github-project <akashlj2@gmail.com>
2022-03-12 11:44:49 +0530

    before Experimenting with grid

--- src/components/subServices/plansList.js
+++ src/components/subServices/plansList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import apires from "../../otherData/api_finder_response.json";
 import { Tabs, useTabState, Panel } from "@bumaga/tabs";
+import "../../css/planList.css";
 
 const cn = (...args) => args.filter(Boolean).join(" ");
 
@@ -27,8 +28,9 @@ const PlansList = () => {
 
 	return (
 		<Tabs state={[state, setState]}>
-			<div className="flex">
-				<div className=" flex flex-col">
+			<div className="grid grid-cols-12 w-full gap-1">
+				<div></div>
+				<div className=" flex flex-col lg:col-span-2">
 					{
 						<div className="p-1 flex flex-col border">
 							{planTypes.map(planType => (
@@ -37,27 +39,33 @@ const PlansList = () => {
 						</div>
 					}
 				</div>
-				<div className="">
-					<h2>some company plans</h2>
-					<div className="flex flex-col border-2 border-gray-700 ">
+				<div className="lg:col-span-9">
+					<h2 className="w-full">some company plans</h2>
+					<div className="w-full flex justify-around items-center">
+						<div>Description</div>
+						<div>Data</div>
+						<div>Validity</div>
+						<div>Amount</div>
+					</div>
+					<div className="flex flex-col h-96 overflow-auto text-sm text-gray-primary">
 						{plansList.map(plan => (
 							<Panel>
-								<div className="grid borderitems-center w-[50rem]">
+								<div className="grid items-center text-gray-primary">
 									{plan.map(eachPlan => (
-										<div className="flex col-span-full w-full border items-center justify-center">
-											<div className="p-1 text-gray-primary w-[25rem] ">
+										<div className="flex col-span-full w-full border-b items-center justify-around text-inherit">
+											<div className="p-1 text-gray-primary w-[25rem] text-inherit">
 												{eachPlan.benefit}
 											</div>
-											<div className="p-1 text-gray-primary w-[10rem]">
+											<div className="p-1 text-gray-primary w-[10rem] text-inherit">
 												{eachPlan.validity}
 											</div>
-											<div className="p-1 text-gray-primary   w-[5rem]">
+											<div className="p-1 text-gray-primary   w-[5rem] text-inherit">
 												{eachPlan.data}
 											</div>
-											<div className="p-1 w-[5rem]">
-												<button className=" mx-auto border border-pink-primary px-3 py-2 hover:bg-pink-primary hover:text-white ">
+											<div className="p-1 w-[5rem] text-pink-primary">
+												<button className=" mx-auto border border-pink-primary w-[75px] hover:bg-pink-primary hover:text-white rounded text-inherit p-1">
 													<span className="mx-auto text-inherit hover:text-white">
-														{eachPlan.amount}
+														Rs {eachPlan.amount}
 													</span>
 												</button>
 											</div>
