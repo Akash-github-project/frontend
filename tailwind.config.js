ab9d3375f4c3917e1ce678e5560a0aca90ba3b0c
Akash Sinha <akashlj2@gmail.com>
2022-04-07 19:17:39 +0530

    testing with patch

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,36 @@
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	theme: {
-		screens: {
-			sm: "200px",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: "200px",
 
-			md: "576px",
+      md: "576px",
 
-			lg: "820px",
+      lg: "820px",
 
-			xl: "992px",
+      xl: "992px",
 
-			"2xl": "1200px",
-		},
-		extend: {
-			spacing: {
-				ms: "13px",
-			},
-			colors: {
-				pink: {
-					primary: "#f5317c",
-				},
-				gray: {
-					primary: "#535b61",
-					separator: "#ccbebe",
-				},
-				green: {
-					info: "#009688",
-				},
-			},
-			boxShadow: {
-				default: "0 0 15px rgba(0, 0, 0, 0.3)",
-			},
-		},
-	},
-};
+      "2xl": "1200px",
+    },
+    extend: {
+      spacing: {
+        ms: "13px",
+      },
+      colors: {
+        pink: {
+          primary: "#f5317c",
+        },
+        gray: {
+          primary: "#535b61",
+          separator: "#ccbebe",
+        },
+        green: {
+          info: "#009688",
+        },
+      },
+      boxShadow: {
+        default: "0 0 15px rgba(0, 0, 0, 0.3)",
+      },
+    },
+  },
+}
